9220c321abbc41679c712236fa6436ace2b26434
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-04-20 22:33:15 +0600

    all ticket implemented

--- db/db.js
+++ db/db.js
@@ -10,7 +10,7 @@ class MyDb {
    *create and save a new ticket
    * @param {string} username
    * @param {number} price
-   * @param {Ticket} return a ticket object
+   * @returns {Ticket} return a ticket object
    */
 
   create(username, price) {
@@ -24,14 +24,87 @@ class MyDb {
    * @param {string} username
    * @param {number} price
    * @param {number} quantity
+   * @returns {Array<Ticket>}
    */
-  bulkCreate(username, price, quantity) {}
+  bulkCreate(username, price, quantity) {
+    const result = [];
+    for (let i = 0; i < quantity; i++) {
+      const ticket = this.create(username, price);
+      result.push(ticket);
+    }
+    return result;
+  }
 
-  //return all ticket
-  find() {}
+  /**
+   * return all available ticket
+   */
+  find() {
+    return this.tickets;
+  }
 
-  //singleTicket
-  findById() {}
+  /**
+   *find ticket by ticket id
+   * @param {string} ticketId
+   * @returns {Ticket} ticket
+   */
+  findById(ticketId) {
+    const ticket = this.tickets.find(
+      /**
+       *
+       * @param {Ticket} ticket
+       */
+      (ticket) => ticket.id === ticketId
+    );
+    return ticket;
+  }
+  /**
+   * find all tickets for a given username
+   * @param {string} username
+   * @returns {Array<Ticket>}
+   */
+  findByUserName(username) {
+    const tickets = this.tickets.filter(
+      /**
+       * @param {Ticket} ticket
+       */
+      (ticket) => ticket.username === username
+    );
+    return tickets;
+  }
+  /**
+   *
+   * @param {string} ticketId
+   * @param {{username: string, price: number}} ticketBody
+   * @returns {Ticket}
+   */
+  updateById(ticketId, ticketBody) {
+    const ticket = this.findById(ticketId);
+    ticket.username = ticketBody.username ?? ticket.username;
+    ticket.price = ticketBody.price ?? ticket.price;
+    ticket.updateAt = new Date();
+
+    return ticket;
+  }
+
+  /**
+   *@param {string} ticketId
+   */
+  deleteById(ticketId) {
+    const index = this.tickets.findIndex((ticket) => ticket.id === ticketId);
+    if (index !== -1) {
+      this.tickets.splice(index, 1);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   *find winner
+   * @param {number} winnerCount
+   * @returns {Array<Ticket>}
+   */
+  draw(winnerCount) {}
 }
 
 const myDb = new MyDb();
